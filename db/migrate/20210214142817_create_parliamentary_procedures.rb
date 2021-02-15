3a520e10deef43520b1fdf4d58de752a7add55a3
Michael Smethurst <michael@smethur.st>
2021-02-15 14:39:10 +0000

    not used

--- db/migrate/20210214142817_create_parliamentary_procedures.rb
+++
@@ -1,8 +0,0 @@
-class CreateParliamentaryProcedures < ActiveRecord::Migration
-  def change
-    create_table :parliamentary_procedures do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
--- db/migrate/20210214145506_create_step_types.rb
+++
@@ -1,8 +0,0 @@
-class CreateStepTypes < ActiveRecord::Migration
-  def change
-    create_table :step_types do |t|
-
-      t.timestamps null: false
-    end
-  end
-end
