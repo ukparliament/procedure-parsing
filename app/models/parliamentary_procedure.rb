dc47a65d047403d9f8a3b1deae2145ead17295ba
Michael Smethurst <michael@smethur.st>
2021-02-16 22:11:53 +0000

    start steps now returns step type name

--- app/models/parliamentary_procedure.rb
+++ app/models/parliamentary_procedure.rb
@@ -5,6 +5,6 @@ class ParliamentaryProcedure < ActiveRecord::Base
   has_many :work_packages
   
   def start_steps
-    Step.all.select('s.*' ).joins( 'as s, step_collections as sc, step_collection_types as sct' ).where( 's.id = sc.step_id' ).where( 'sc.step_collection_type_id = sct.id' ).where( 'sct.name = ?', 'Start steps' ).where( 'sc.parliamentary_procedure_id =?', self )
+    Step.all.select('s.*, st.name as step_type_name' ).joins( 'as s, step_collections as sc, step_collection_types as sct, step_types as st' ).where( 's.id = sc.step_id' ).where( 'sc.step_collection_type_id = sct.id' ).where( 'sct.name = ?', 'Start steps' ).where( 'sc.parliamentary_procedure_id =?', self ).where( 's.step_type_id = st.id' )
   end
 end
