d1b004cc322010874239806d9e9bbf45ce7541f5
Michael Smethurst <michael@smethur.st>
2021-02-16 16:04:35 +0000

    added method to get start steps

--- app/models/parliamentary_procedure.rb
+++ app/models/parliamentary_procedure.rb
@@ -3,4 +3,8 @@ class ParliamentaryProcedure < ActiveRecord::Base
   has_many :procedure_routes
   has_many :routes, :through => 'procedure_routes'
   has_many :work_packages
+  
+  def start_steps
+    Step.all.select('s.*' ).joins( 'as s, step_collections as sc, step_collection_types as sct' ).where( 's.id = sc.step_id' ).where( 'sc.step_collection_type_id = sct.id' ).where( 'sct.name = ?', 'Start steps' ).where( 'sc.parliamentary_procedure_id =?', self )
+  end
 end
