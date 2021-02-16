f5af52afde15a3efec79e5a829120e7f2e8a2d09
Michael Smethurst <michael@smethur.st>
2021-02-16 13:36:25 +0000

    methods to get business items by date

--- app/models/work_package.rb
+++ app/models/work_package.rb
@@ -1,2 +1,14 @@
 class WorkPackage < ActiveRecord::Base
+  
+  def business_items_that_have_happened
+    BusinessItem.all.where( 'work_package_id = ?', self).where( 'date <= ?', Date.today ).order( 'date' )
+  end
+  
+  def business_items_that_are_scheduled_to_happen
+    BusinessItem.all.where( 'work_package_id = ?', self).where( 'date > ?', Date.today ).order( 'date' )
+  end
+  
+  def business_items_unknown
+    BusinessItem.all.where( 'work_package_id = ?', self).where( 'date is null' ).order( 'date' )
+  end
 end
