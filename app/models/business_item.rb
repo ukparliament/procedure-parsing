fcf124f011f7b35a2deff30a526cd41545532ca9
Michael Smethurst <michael@smethur.st>
2021-02-16 16:03:57 +0000

    added associations to actualisations and steps. method to get step names

--- app/models/business_item.rb
+++ app/models/business_item.rb
@@ -1,2 +1,17 @@
 class BusinessItem < ActiveRecord::Base
+  
+  has_many :actualisations
+  has_many :steps, :through => 'actualisations'
+  
+  def step_names
+    step_names = ''
+    if self.steps.size == 1
+      step_names += self.steps.first.name
+    else
+      self.steps.each do |step|
+        step_names += step.name + ' <br /> '
+      end
+    end
+    step_names
+  end
 end
