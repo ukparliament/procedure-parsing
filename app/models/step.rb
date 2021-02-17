24ac79323b2cbe9e58b4924224ebd700e9f072e3
Michael Smethurst <michael@smethur.st>
2021-02-17 10:17:48 +0000

    added spacing. no real change

--- app/models/step.rb
+++ app/models/step.rb
@@ -1,6 +1,6 @@
 class Step < ActiveRecord::Base
   
   def outbound_routes_in_procedure( procedure )
-    Route.all.select( 'r.*' ).joins( 'as r, procedure_routes as pr').where( 'r.from_step_id = ?', self ).where( 'r.id = pr.route_id' ).where( 'pr.parliamentary_procedure_id = ?', procedure )
+    Route.all.select( 'r.*' ).joins( 'as r, procedure_routes as pr' ).where( 'r.from_step_id = ?', self ).where( 'r.id = pr.route_id' ).where( 'pr.parliamentary_procedure_id = ?', procedure )
   end
 end
