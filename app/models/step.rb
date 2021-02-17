536882738354f1db235d650882bdb270dda90232
Michael Smethurst <michael@smethur.st>
2021-02-17 13:07:03 +0000

    added methids to get inbound routes in procedure and whether it's been actualised in procedure

--- app/models/step.rb
+++ app/models/step.rb
@@ -3,4 +3,12 @@ class Step < ActiveRecord::Base
   def outbound_routes_in_procedure( procedure )
     Route.all.select( 'r.*' ).joins( 'as r, procedure_routes as pr' ).where( 'r.from_step_id = ?', self ).where( 'r.id = pr.route_id' ).where( 'pr.parliamentary_procedure_id = ?', procedure )
   end
+  
+  def inbound_routes_in_procedure( procedure )
+    Route.all.select( 'r.*' ).joins( 'as r, procedure_routes as pr' ).where( 'r.to_step_id = ?', self ).where( 'r.id = pr.route_id' ).where( 'pr.parliamentary_procedure_id = ?', procedure )
+  end
+  
+  def actualised_has_happened_in_work_package?( work_package )
+    BusinessItem.all.select( 'bi.*' ).joins( 'as bi, actualisations as a' ).where( 'a.step_id = ?', self ).where( 'a.business_item_id = bi.id' ).where( 'bi.work_package_id  ?', work_package ).where( 'bi.date >= ?', Date.today )
+  end
 end
