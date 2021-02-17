a83f05d554a7b95fc295882e07120411c13a9100
Michael Smethurst <michael@smethur.st>
2021-02-17 15:48:15 +0000

    moved stuff into methods

--- app/controllers/work_package_controller.rb
+++ app/controllers/work_package_controller.rb
@@ -1,54 +1,39 @@
 # Individual parsing rules for steps types are are packaged into separate files. This code requires those files to be loaded.
 require 'parsing/business_step'
-#require 'parsing/decision_step'
-#require 'parsing/not_step'
+require 'parsing/decision_step'
+require 'parsing/not_step'
 require 'parsing/and_step'
-#require 'parsing/or_step'
+require 'parsing/or_step'
 
 class WorkPackageController < ApplicationController
   
   # Include code from each of the modules for the different styles of parsing.
   include PARSING_BUSINESS_STEP
-  #include PARSING_DECISION_STEP
-  #include PARSING_NOT_STEP
+  include PARSING_DECISION_STEP
+  include PARSING_NOT_STEP
   include PARSING_AND_STEP
-  #include PARSING_OR_STEP
+  include PARSING_OR_STEP
   
   def show
     work_package = params[:work_package]
     @work_package = WorkPackage.find( work_package )
   end
   
+  # This method attempts to parse a procedure in the context of a work package.
   def parse
+    
+    # Get the work package we're trying to parse.
     work_package = params[:work_package]
     @work_package = WorkPackage.find( work_package )
     
+    # Get the procedure the work package is subject to.
     procedure = @work_package.parliamentary_procedure
     
-    # Get all the routes in the procedure the work package is subject to.
-    routes = @work_package.parliamentary_procedure.routes_with_steps
-    
-    # Create a hash to hold the routes and their attributes
-    @routes = {}
-    
-    # Loop through the routes and ...
-    routes.each do |route|
-      
-      # ... create a hash of values we want to store for the route
-      route_hash = create_route_hash(
-        route,
-        'NULL',
-        'UNPARSED',
-        false,
-        route.source_step_name,
-        route.source_step_type,
-        route.target_step_name,
-        route.target_step_type
-      )
-    end
+    # Initialise a hash of additional route attributes keyed off the route.
+    initialise_route_hash( @work_package )
     
     # Get the start steps of the procedure the work package is subject to.
-    start_steps = @work_package.parliamentary_procedure.start_steps
+    start_steps = procedure.start_steps
     
     # Loop through the start steps.
     start_steps.each do |step|
@@ -63,17 +48,10 @@ end
 # We pass in the route to be parsed, the source step of that route and the procedure the route is in
 def parse_route( route, source_step, procedure )
   
-  
-
-  # HACK
-  # This is a temp hack to stop parsing
-  update_route_hash( route, nil, nil, true, nil, nil, nil, nil )
-  # HACK
-  
-  # Get inbound routes to the source step
+  # Get inbound routes to the source step.
   inbound_routes = source_step.inbound_routes_in_procedure( procedure )
   
-  # Check the type of the source step
+  # Check the type of the source step.
   case @routes[route][:source_step_type]
   when "Business step"
     parse_route_from_business_step( route, source_step, procedure, inbound_routes )
@@ -87,8 +65,6 @@ def parse_route( route, source_step, procedure )
     parse_route_from_or_step( route, source_step, procedure, inbound_routes )
   end
   
-
-  
   # If the route is current...
   if route.current
     
@@ -99,14 +75,8 @@ def parse_route( route, source_step, procedure )
   else
     
     # ... update the route current attribute to 'FALSE', the status attribute to 'UNTRAVERSABLE' and the parsed attribute to 'TRUE'
-    update_route_hash( route, 'FALSE', 'UNTRAVERSABLE', 'TRUE', nil, nil, nil, nil )
+    update_route_hash( route, 'FALSE', 'UNTRAVERSABLE', true, nil, nil, nil, nil )
   end
-  
-  
-  
-  
-  
-  
   
   # Get the target step of the route.
   target_step = route.target_step
@@ -118,7 +88,7 @@ def parse_route( route, source_step, procedure )
     unless @routes[outbound_route][:parsed] == true
       
       # ...parse the route.
-      parse_route( outbound_route, target_step, procedure ) 
+      parse_route( outbound_route, target_step, procedure )
     end
   end
 end
@@ -128,8 +98,31 @@ end
 
 
 
-
-
+# Method to initialise a hash of route attributes keyed off the route.
+def initialise_route_hash( work_package )
+  
+  # Get all the routes in the procedure the work package is subject to.
+  routes = work_package.parliamentary_procedure.routes_with_steps
+  
+  # Create a hash to hold the routes and their attributes
+  @routes = {}
+  
+  # Loop through the routes and ...
+  routes.each do |route|
+    
+    # ... create a hash of values we want to store for the route
+    route_hash = create_route_hash(
+      route,
+      'NULL',
+      'UNPARSED',
+      false,
+      route.source_step_name,
+      route.source_step_type,
+      route.target_step_name,
+      route.target_step_type
+    )
+  end
+end
 
 # Method to create a hash of route attributes and add it to the route hash
 def create_route_hash( route, current, status, parsed, source_step_name, source_step_type, target_step_name, target_step_type )
