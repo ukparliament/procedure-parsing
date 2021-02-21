b2156cdc28157c91c9cd2841c580d8fab03dd7b0
Michael Smethurst <michael@smethur.st>
2021-02-21 16:43:25 +0000

    added house or houses to step names method

--- app/models/business_item.rb
+++ app/models/business_item.rb
@@ -6,10 +6,10 @@ class BusinessItem < ActiveRecord::Base
   def step_names
     step_names = ''
     if self.steps.size == 1
-      step_names += self.steps.first.name
+      step_names += self.steps.first.name + ' (' + self.steps.first.house_label + ')'
     else
       self.steps.each do |step|
-        step_names += step.name + ' <br /> '
+        step_names += step.name + ' (' + self.steps.first.house_label + ')' + ' <br /> '
       end
     end
     step_names
