f72998a443dc8e08ac85e4b4774d46dc73448970
Michael Smethurst <michael@smethur.st>
2021-02-21 16:46:20 +0000

    methods to append houses to end of step label

--- app/models/step.rb
+++ app/models/step.rb
@@ -11,4 +11,26 @@ class Step < ActiveRecord::Base
   def actualised_has_happened_in_work_package?( work_package )
     BusinessItem.all.select( 'bi.*' ).joins( 'as bi, actualisations as a' ).where( 'a.step_id = ?', self ).where( 'a.business_item_id = bi.id' ).where( 'bi.work_package_id  ?', work_package ).where( 'bi.date >= ?', Date.today )
   end
+  
+  def in_commons?
+    HouseStep.all.select( 'hs.*' ).joins( 'as hs, houses as h' ).where( 'hs.step_id = ?', self ).where( 'hs.house_id = h.id').where( 'h.name =?', 'House of Commons' ).order( 'hs.id' ).first
+  end
+  
+  def in_lords?
+    HouseStep.all.select( 'hs.*' ).joins( 'as hs, houses as h' ).where( 'hs.step_id = ?', self ).where( 'hs.house_id = h.id').where( 'h.name =?', 'House of Lords' ).order( 'hs.id' ).first
+  end
+  
+  def house_label
+    house_label = ''
+    if self.in_commons? and self.in_lords?
+      house_label = 'House of Commons and House of Lords'
+    elsif self.in_commons?
+      house_label = 'House of Commons'
+    elsif self.in_lords?
+      house_label = 'House of Lords'
+    else
+      house_label = 'Neither House'
+    end
+    house_label
+  end
 end
