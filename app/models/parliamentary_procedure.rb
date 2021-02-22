4af692b77e9e1cecc01864f35cafffbe61b14b6b
Robert Brook <robertbrook@fastmail.fm>
2021-02-22 23:41:45 +0000

    more markup

--- app/models/parliamentary_procedure.rb
+++ app/models/parliamentary_procedure.rb
@@ -3,6 +3,10 @@ class ParliamentaryProcedure < ActiveRecord::Base
   has_many :procedure_routes
   has_many :routes, :through => 'procedure_routes'
   has_many :work_packages
+
+  def description_massaged
+    description.length < 256 ? description : description + "..."
+  end
   
   def start_steps
     Step.all.select('s.*, st.name as step_type_name' ).joins( 'as s, step_collections as sc, step_collection_types as sct, step_types as st' ).where( 's.id = sc.step_id' ).where( 'sc.step_collection_type_id = sct.id' ).where( 'sct.name = ?', 'Start steps' ).where( 'sc.parliamentary_procedure_id =?', self ).where( 's.step_type_id = st.id' )
