893195e791c89f7e3cf2c1ba2e0d1cea6452edb5
Robert Brook <robertbrook@fastmail.fm>
2021-02-24 10:52:02 +0000

    trying default xml and json output

--- app/controllers/procedure_controller.rb
+++ app/controllers/procedure_controller.rb
@@ -1,16 +1,24 @@
 class ProcedureController < ApplicationController
-  
+
   def index
     @procedures = ParliamentaryProcedure.all.order( 'name' )
+
+    respond_to do |format|
+      format.html
+      format.xml { render xml: @procedures }
+      format.json { render json: @procedures }
+    end
   end
   
   def show
     procedure = params[:procedure]
     @procedure = ParliamentaryProcedure.find( procedure )
+
   end
   
   def work_package_index
     procedure = params[:procedure]
     @procedure = ParliamentaryProcedure.find( procedure )
+    
   end
 end
