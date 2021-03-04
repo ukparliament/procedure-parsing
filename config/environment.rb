7d1ad6063096141218f7bb96f85fae74f6cc126c
Michael Smethurst <michael@smethur.st>
2021-03-04 21:01:04 +0000

    post house move tidy

--- config/environment.rb
+++ config/environment.rb
@@ -1,3 +1,9 @@
+DB_HOST="peerages.postgres.database.azure.com"
+DB_DATABASE="procedures"
+DB_USERNAME="peerageuser@peerages"
+DB_PASSWORD="448aada9-168c-4bb9-bd84-ccdf46b0144a"
+
+
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
