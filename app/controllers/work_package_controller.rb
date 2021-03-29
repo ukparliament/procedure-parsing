01c7896d2ec1f4b4bd0354f7ebf2200cba600be6
Michael Smethurst <michael@smethur.st>
2021-03-29 21:30:26 +0100

    rejigged comments

--- app/controllers/work_package_controller.rb
+++ app/controllers/work_package_controller.rb
@@ -10,7 +10,7 @@ require "#{Rails.root}/lib/parsing/from-start-steps/or_step"
 
 class WorkPackageController < ApplicationController
   
-  # We include code for the different styles of parsing according to the source step type.
+  ## We include code for the different styles of parsing according to the source step type.
   include PARSE_FROM_START_STEPS
   include PARSE_BUSINESS_STEP_FROM_START_STEPS
   include PARSE_DECISION_STEP_FROM_START_STEPS
@@ -25,60 +25,47 @@ class WorkPackageController < ApplicationController
     parse
   end
   
-  # This method attempts to parse a procedure in the context of a work package.
+  ## This method attempts to parse a work package subject to a procedure.
   def parse
     
-    # We get the work package we're trying to parse.
+    # We get the work package we're attempting to parse.
     work_package = params[:work_package]
     @work_package = WorkPackage.find( work_package )
   
     # We get the procedure the work package is subject to.
     procedure = @work_package.parliamentary_procedure
   
-    # We tell the user what we're attempting to do.
-    puts "Attempting to parse work package #{@work_package.id}, subject to the #{procedure.name.downcase} procedure."
+    # We set the parse count to zero.
+    # This will be incremented every time we parse a route and used for reporting.
+    # The parse count is created as an instance variable because we want to increment on each parse and report from it later.
+    @parse_count = 0
     
     # We set up an array to log the parsing.
-    # Create as an instance variable because we want to write to and from it later.
+    # The log is created as an instance variable because we want to write to it and report from it later.
     @parse_log = []
-  
-    # We create a set of arrays to store the target business steps of the routes we parse according to the value of the inbound route once parsed.
-    # Created as instance variables because we want to write to and from them later.
+    
+    # We write to the log, explaining what we're attempting to do.
+    @parse_log << "Attempting to parse work package #{@work_package.id}, subject to the #{procedure.name.downcase} procedure."
+    
+    # We create a set of arrays to store the target business steps of the routes we parse according to the status of the inbound route once parsed.
+    # Theese are created as instance variables because we want to write to then and report from them later.
     @caused_steps = []
     @allowed_steps = []
     @disallowed_yet_steps = []
     @disallowed_now_steps = []
   
-    # We initialise a hash of additional route attributes keyed off the route.
+    # We initialise a hash of route attributes keyed off the route.
+    # This is used to store the progress and results of the route parsing for each route in the procedure.
     initialise_route_hash( @work_package )
-  
-    # We set the parse count to zero.
-    # This will be incremented every time we parse a route and used for reporting.
-    # Created as an instance variable because we want to increment on each parse and report on it later.
-    @parse_count = 0
-  
-    # We get the all the business steps in the procedure the work package is subject to.
-    #business_steps = procedure.business_steps
-  
-    # We loop through the business steps ...
-    #business_steps.each do |step|
     
-      # ... and loop through the outbound routes of each business step ...
-      #step.outbound_routes_in_procedure( procedure ).each do |route|
-      
-        # ... and parse each route.
-        #parse_route( route, step, procedure )
-        #end
-    #end
-    
-    # We get the all start steps of the procedure the work package is subject to.
-    # Created as an instance variable because we want to check it when parsing business steps.
+    # We get an array of the start steps in the procedure the work package is subject to.
+    # The array is created as an instance variable because we want to check it when parsing business steps.
     @start_steps = procedure.start_steps
   
-    # We loop through the start steps ...
+    # We loop through the start steps in the procedure ...
     @start_steps.each do |step|
     
-      # ... and loop through the outbound routes of the start steps ...
+      # ... loop through the outbound routes of each start step ...
       step.outbound_routes_in_procedure( procedure ).each do |route|
     
         # ... and parse each route.
