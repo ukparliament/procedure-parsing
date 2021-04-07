d1b4d4ed56f6ec6ac197338c42c4885f76b80424
Michael Smethurst <michael@smethur.st>
2021-04-07 20:10:50 +0100

    added basic comments

--- lib/parsing/assign_potential_business_step_state.rb
+++ lib/parsing/assign_potential_business_step_state.rb
@@ -1,42 +1,40 @@
 module PARSE_ASSIGN_POTENTIAL_BUSINESS_STEP_STATE
   
+  # # A method to assign a [potential state](https://ukparliament.github.io/ontologies/procedure/flowcharts/meta/design-notes/#potential-states-of-a-business-step) to a business step, being the target of a parsed route.
   def assign_potential_business_step_state( route )
-    # ## ... we assign the potential state of any target business step.
+    
     # If the route we're attempting to parse has been fully parsed ...
     if @routes[route][:parsed] == true
       
       # ... we write to the parse logger, reporting the route's parsed status.
       @parse_log << "Successfully parsed as <strong>#{@routes[route][:status]}</strong>."
-      
-      
-      # Modularise this?
     
-      # ... if the target step of the route we've just parsed is a business step ...
+      # ... if the target step of the route we've parsed is a business step ...
       if route.target_step.step_type_name == 'Business step'
     
-        # ... if the status of the route we've just parsed is 'TRUE' ...
+        # ... if the status of the route we've parsed is 'TRUE' ...
         if @routes[route][:status] == 'TRUE'
       
           # ... we add the target step to the array of caused steps, unless it is already in that array.
-          @caused_steps << route.target_step
+          @caused_steps << route.target_step unless @caused_steps.include?( route.target_step )
       
-        # ... if the status of the route we've just parsed is 'ALLOWS' ...
+        # ... if the status of the route we've parsed is 'ALLOWS' ...
         elsif @routes[route][:status] == 'ALLOWS'
       
           # ... we add the target step to the array of allowed steps, unless it is already in that array.
-          @allowed_steps << route.target_step
+          @allowed_steps << route.target_step unless @allowed_steps.include?( route.target_step )
       
-        # ... if the status of the route we've just parsed is 'FALSE' or 'NULL' ...
+        # ... if the status of the route we've parsed is 'FALSE' or 'NULL' ...
         elsif @routes[route][:status] == 'NULL' or  @routes[route][:status] == 'FALSE'
       
           # ... we add the target step to the array of disallowed as yet steps, unless it is already in that array.
-          @disallowed_yet_steps << route.target_step
+          @disallowed_yet_steps << route.target_step unless @disallowed_yet_steps.include?( route.target_step )
       
-        # ... if the status of the route we've just parsed is 'UNTRAVERSABLE' ...
+        # ... if the status of the route we've parsed is 'UNTRAVERSABLE' ...
         elsif @routes[route][:status] == 'UNTRAVERSABLE'
       
           # ... we add the target step to the array of disallowed now steps, unless it is already in that array.
-          @disallowed_now_steps << route.target_step
+          @disallowed_now_steps << route.target_step unless @disallowed_now_steps.include?( route.target_step )
         end
       end
     end
