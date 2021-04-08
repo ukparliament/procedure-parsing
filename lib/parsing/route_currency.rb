a9390f825b89f7e4e90250e855f08f2928f009ed
Michael Smethurst <michael@smethur.st>
2021-04-08 15:31:08 +0100

    first pass comments

--- lib/parsing/route_currency.rb
+++ lib/parsing/route_currency.rb
@@ -1,22 +1,22 @@
+# # Module to determine whether a route is currently traversible.
 module PARSE_ROUTE_CURRENCY
-  
+
+  # ## Method to determine whether a route is currently traversible.
   def parse_route_currency( route )
-
-
-    # ### We check for route currency.
-
-    # A non-current route is one with a start date that is later than today or one with an end date that is today or earlier than today.
+    
+    # A route has optional start and end dates.
+    # A non-current route is one with a start date that is later than today or one with an end date of today or earlier than today.
     # If the route is current...
     if route.current
 
       # ... we update the route current attribute to true.
       update_route_hash( route, 'TRUE', nil, nil, nil, nil, nil, nil, nil )
 
-    # ... otherwise, if the route is not current ...
+    # Otherwise, if the route is not current ...
     else
 
       # ... we update the route current attribute to 'FALSE' and the route status attribute to 'UNTRAVERSABLE' ...
-      # ... setting the route status to 'UNTRAVERSABLE' records that this route in not currently active and is used to infer that routes from this route are also not currently active.
+      # ... setting the route status to 'UNTRAVERSABLE' records that this route is not currently traversible and is used to infer that routes from this route are also not currently traversible.
       # ... we also record this route as parsed because we don't want to visit it and attempt to parse again.
       update_route_hash( route, 'FALSE', 'UNTRAVERSABLE', true, nil, nil, nil, nil, nil )
     end
