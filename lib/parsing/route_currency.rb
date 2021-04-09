8cc5ab539a0b9312ef26c84747e9748f6c01a1d5
Michael Smethurst <michael@smethur.st>
2021-04-09 14:16:26 +0100

    comment edits with robert:

--- lib/parsing/route_currency.rb
+++ lib/parsing/route_currency.rb
@@ -1,22 +1,21 @@
-# # Module to determine whether a route is currently traversible.
+# # A module to determine whether a route is current.
 module PARSE_ROUTE_CURRENCY
 
-  # ## Method to determine whether a route is currently traversible.
+  # ## A method to determine whether a route is current.
   def parse_route_currency( route )
-    
     
     # If the route is current...
     if route.current
 
-      # ... we update the route current attribute to true.
+      # ... we update the route current attribute to TRUE.
       update_route_hash( route, 'TRUE', nil, nil, nil, nil, nil, nil, nil )
 
     # Otherwise, if the route is not current ...
     else
 
-      # ... we update the route current attribute to 'FALSE' and the route status attribute to 'UNTRAVERSABLE' ...
-      # ... setting the route status to 'UNTRAVERSABLE' records that this route is not currently traversible and is used to infer that routes from this route are also not currently traversible.
-      # ... we also record this route as parsed because we don't want to visit it and attempt to parse again.
+      # ... we update the route current attribute to FALSE and the route status attribute to UNTRAVERSABLE.
+      # Setting the route status to UNTRAVERSABLE records that the route is not currently traversable - and is used to infer that routes which follow this route are also not currently traversable.
+      # We also record this route as parsed because we don't want to visit it and attempt to parse again.
       update_route_hash( route, 'FALSE', 'UNTRAVERSABLE', true, nil, nil, nil, nil, nil )
     end
   end
---
+++ public/comments/route_currency.rb.md
@@ -0,0 +1,25 @@
+# A module to determine whether a route is current.
+
+module PARSE_ROUTE_CURRENCY
+## A method to determine whether a route is current.
+
+  def parse_route_currency( route )
+If the route is current...
+
+    if route.current
+... we update the route current attribute to TRUE.
+
+      update_route_hash( route, 'TRUE', nil, nil, nil, nil, nil, nil, nil )
+Otherwise, if the route is not current ...
+
+    else
+... we update the route current attribute to FALSE and the route status attribute to UNTRAVERSABLE.
+
+Setting the route status to UNTRAVERSABLE records that the route is not currently traversable - and is used to infer that routes which follow this route are also not currently traversable.
+
+We also record this route as parsed because we don't want to visit it and attempt to parse again.
+
+      update_route_hash( route, 'FALSE', 'UNTRAVERSABLE', true, nil, nil, nil, nil, nil )
+    end
+  end
+end
