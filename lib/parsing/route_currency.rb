04f56bffc9e0d8269a31fe6def5281fefdebbd0e
Michael Smethurst <michael@smethur.st>
2021-04-13 18:05:41 +0100

    no longer passes source and target step name and type to update_route_hash method

--- lib/parsing/route_currency.rb
+++ lib/parsing/route_currency.rb
@@ -8,7 +8,7 @@ module PARSE_ROUTE_CURRENCY
     if route.current
 
       # ... we update the route current attribute to TRUE.
-      update_route_hash( route, 'TRUE', nil, nil, nil, nil, nil, nil, nil )
+      update_route_hash( route, 'TRUE', nil, nil, nil )
 
     # Otherwise, if the route is not current ...
     else
@@ -16,7 +16,7 @@ module PARSE_ROUTE_CURRENCY
       # ... we update the route current attribute to FALSE and the route status attribute to UNTRAVERSABLE.
       # Setting the route status to UNTRAVERSABLE records that the route is not currently traversable - and is used to infer that routes which follow this route are also not currently traversable.
       # We also record this route as parsed because we don't want to visit it and attempt to parse again.
-      update_route_hash( route, 'FALSE', 'UNTRAVERSABLE', true, nil, nil, nil, nil, nil )
+      update_route_hash( route, 'FALSE', 'UNTRAVERSABLE', true, nil )
     end
   end
 end
