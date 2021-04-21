1a8002dea4bebfa74d21d4f4f9dd39a11253efc6
Michael Smethurst <michael@smethur.st>
2021-04-21 12:05:49 +0100

    rejigged comments with robert

--- lib/parsing/not_step.rb
+++ lib/parsing/not_step.rb
@@ -4,14 +4,14 @@ module PARSE_NOT_STEP
   # ## Method to parse a route whose source step is a NOT step.
   def parse_route_from_not_step( route, source_step, inbound_routes )
     
-    # Design note: The [method used](https://ukparliament.github.io/ontologies/procedure/flowcharts/meta/design-notes/#validating-inputs-and-outputs-to-steps) for validating the number of input and output route for each step type.
-    # If the NOT step does not have exactly one inbound route ...
+    # Design note: The [method used](https://ukparliament.github.io/ontologies/procedure/flowcharts/meta/design-notes/#validating-inputs-and-outputs-to-steps) for validating the number of input and output routes for each step type.
+    # If the NOT step does not have one inbound route ...
     if inbound_routes.size != 1
   
-      # ... flag the step has an unexpected number of routes.
+      # ... log the step as has having an unexpected number of routes.
       logger.error "NOT step with ID #{source_step.id} has #{inbound_routes.size} inbound routes."
   
-    # Otherwise, if the NOT step has exactly one inbound route ...
+    # Otherwise, if the NOT step does have one inbound route ...
     else
   
       # ... if the inbound route to the source step has been parsed ....
@@ -22,29 +22,29 @@ module PARSE_NOT_STEP
         
         # ... we refer to the [NOT step truth table](https://ukparliament.github.io/ontologies/procedure/flowcharts/meta/design-notes/#truth-table-not) ...
         
-        # ... we check the status of the inbound route to the source step ...
+        # ... we check the status of the inbound route to the source step.
         case @routes[inbound_routes[0]][:status]
-        # ... and if the status of the inbound route to the source step is 'TRUE' ...
+        
+        # When the status of the inbound route to the source step is 'TRUE' ...
         when 'TRUE'
           
           # ... we set the status of this route to 'FALSE'.
           update_route_hash( route, nil, 'FALSE', nil, nil )
           
-        # ... and if the status of the inbound route to the source step is 'FALSE' ...
+        # When the status of the inbound route to the source step is 'FALSE' ...
         when 'FALSE'
           
           # ... we set the status of this route to 'TRUE'.
           update_route_hash( route, nil, 'TRUE', nil, nil )
           
-        # ... and if the status of the inbound route is 'NULL' or 'UNTRAVERSABLE' ...
+        # Otherwise, when the status of the inbound route is neither ‘TRUE’ nor ‘FALSE’ ...
         else
           
           # ... we set the status of this route to the status of the inbound route.
           update_route_hash( route, nil, @routes[inbound_routes[0]][:status], nil, nil )
         end
         
-      # ...otherwise, if the inbound route has not been parsed ...
-      # ... we do nothing and parse on a subsequent pass.
+      # Otherwise, the inbound route is not parsed and will be parsed on a later pass.
       end
     end
   end
