0bf596b7efc504920f7a09caffbf801ab0f52798
Michael Smethurst <michael@smethur.st>
2021-04-29 20:41:37 +0100

    pass in route id not route object. uses target_step_of_route_with_id to assign to potential step states rather than target step object

--- lib/parsing/assign_potential_business_step_state.rb
+++ lib/parsing/assign_potential_business_step_state.rb
@@ -2,49 +2,49 @@
 module PARSE_ASSIGN_POTENTIAL_BUSINESS_STEP_STATE
   
   # ## Method to assign a [potential state](https://ukparliament.github.io/ontologies/procedure/flowcharts/meta/design-notes/#potential-states-of-a-business-step) to a business step, being the target of a successfully parsed route.
-  def assign_potential_business_step_state( route )
+  def assign_potential_business_step_state( route_id )
     
     # If the route we're attempting to parse has been successfully parsed ...
-    if @routes[route.id][:parsed] == true
+    if @routes[route_id][:parsed] == true
       
       # ... we write to the parse log, reporting the route's parsed status.
-      @parse_log << "Successfully parsed as <strong>#{@routes[route.id][:status]}</strong>."
+      @parse_log << "Successfully parsed as <strong>#{@routes[route_id][:status]}</strong>."
       
       # ... if the target step of the route we've parsed is a business step ...
-      if route.target_step.step_type_name == 'Business step'
+      if @routes[route_id][:route].target_step_type == 'Business step'
         
         # ### We check the status of the route we've parsed.
-        case @routes[route.id][:status]
+        case @routes[route_id][:status]
           
         # When the status of the route we've parsed is 'TRUE' ...
         when "TRUE"
           
           # ... we add the target step to the array of caused steps.
-          @caused_steps << route.target_step
+          @caused_steps << target_step_of_route_with_id( route_id )
           
         # When the status of the route we've parsed is 'ALLOWS' ...
         when "ALLOWS"
           
           # ... we add the target step to the array of allowed steps.
-          @allowed_steps << route.target_step
+          @allowed_steps << target_step_of_route_with_id( route_id )
           
         # When the status of the route we've parsed is either 'FALSE' or 'NULL' ...
         when "NULL", "FALSE"
           
           # ... we add the target step to the array of disallowed as yet steps.
-          @disallowed_as_yet_steps << route.target_step
+          @disallowed_as_yet_steps << target_step_of_route_with_id( route_id )
       
         # When the status of the route we've parsed is 'UNTRAVERSABLE' ...
         when "UNTRAVERSABLE"
           
           # ... we add the target step to the array of disallowed now steps.
-          @disallowed_now_steps << route.target_step
+          @disallowed_now_steps << target_step_of_route_with_id( route_id )
           
         # Otherwise, if the status of the route we've parsed is neither 'TRUE', 'ALLOWS', 'FALSE', 'NULL' or 'UNTRAVERSABLE' ...
         else
           
           # ... we write to the parse log, reporting that the successfully parsed route has an unexpected status.
-          @parse_log << "Unexpected status of <strong>#{@routes[route.id][:status]}</strong>."
+          @parse_log << "Unexpected status of <strong>#{@routes[route_id][:status]}</strong>."
         end
       end
     end
