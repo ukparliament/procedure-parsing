5f448edac672024d4d6d39586fc94b52f72439d1
Michael Smethurst <michael@smethur.st>
2021-04-29 21:02:47 +0100

    only passed a route id. uses the routes_to_step hash to query for inbound routes rather than sql

--- lib/parsing/decision_step.rb
+++ lib/parsing/decision_step.rb
@@ -2,37 +2,40 @@
 module PARSE_DECISION_STEP
   
   # ## Method to parse a route whose source step is a decision step.
-  def parse_route_from_decision_step( route, source_step, inbound_routes )
+  def parse_route_from_decision_step( route_id )
     
     # Design note: The [method used](https://ukparliament.github.io/ontologies/procedure/flowcharts/meta/design-notes/#validating-inputs-and-outputs-to-steps) for validating the number of input and output routes for each step type.
     # If the decision step does not have one inbound route ...
-    if inbound_routes.size != 1
+    if @routes_to_steps[@routes[route_id][:route].from_step_id].size != 1
   
       # ... log the step as has having an unexpected number of inbound routes.
-      logger.error "Decision step with ID #{source_step.id} has #{inbound_routes.size} inbound routes."
+      logger.error "Decision step with name #{@routes[route_id][:route].source_step_name} has #{@routes_to_steps[@routes[route_id][:route].from_step_id].size} inbound routes."
   
     # Otherwise, if the decision step does have one inbound route ...
     else
+      
+      # ... we get the ID of the inbound route.
+      inbound_route_id = @routes_to_steps[@routes[route_id][:route].from_step_id][0]
   
       # ... if the inbound route to the source step has been parsed ....
-      if @routes[inbound_routes[0].id][:parsed] == true
+      if @routes[inbound_route_id][:parsed] == true
     
         # ... we update the route parsed attribute to true.
-        update_route_hash( route, nil, nil, true, nil )
+        update_route_hash( route_id, nil, nil, true, nil )
         
         # ... we refer to the [decision step truth table](https://ukparliament.github.io/ontologies/procedure/flowcharts/meta/design-notes/#truth-table-decision) ...
         
         # ... and if the status of the inbound route to the source step is 'TRUE' ...
-        if @routes[inbound_routes[0].id][:status] == 'TRUE'
+        if @routes[inbound_route_id][:status] == 'TRUE'
         
           # ... we set the status of this route to 'ALLOWS'.
-  				update_route_hash( route, nil, 'ALLOWS', nil, nil )
+  				update_route_hash( route_id, nil, 'ALLOWS', nil, nil )
           
         # ... otherwise, if the status of the inbound route to the source step is not ‘TRUE’ ...
         else
           
           # ... we set the status of this route to the status of the inbound route.
-          update_route_hash( route, nil, @routes[inbound_routes[0].id][:status], nil, nil )
+          update_route_hash( route_id, nil, @routes[inbound_route_id][:status], nil, nil )
         end
         
       # Otherwise, the inbound route is not parsed and will be parsed on a later pass.
