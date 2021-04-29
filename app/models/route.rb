8db1dabf167ee65c8a81beb53f95958a7462c1f6
Michael Smethurst <michael@smethur.st>
2021-04-29 20:34:52 +0100

    methods no longer used and commented

--- app/models/route.rb
+++ app/models/route.rb
@@ -9,40 +9,40 @@ class Route < ActiveRecord::Base
   
   # ## A method to get the source step of a route.
   # This method returns the source step and the name of its type, to save on querying for this later.
-  def source_step
-    Step.all.select( 's.*, st.name as step_type_name' ).joins( 'as s, step_types as st' ).where( 's.step_type_id = st.id' ).where( 's.id = ?', self.from_step_id ).order( 's.id' ).first
-  end
+  #def source_step
+    #Step.all.select( 's.*, st.name as step_type_name' ).joins( 'as s, step_types as st' ).where( 's.step_type_id = st.id' ).where( 's.id = ?', self.from_step_id ).order( 's.id' ).first
+    #end
   
   # ## A method to get the target step of a route.
   # This method returns the target step and the name of its type, to save on querying for this later.
-  def target_step
-    Step.all.select( 's.*, st.name as step_type_name' ).joins( 'as s, step_types as st' ).where( 's.step_type_id = st.id' ).where( 's.id = ?', self.to_step_id ).order( 's.id' ).first
-  end
+  #def target_step
+    #Step.all.select( 's.*, st.name as step_type_name' ).joins( 'as s, step_types as st' ).where( 's.step_type_id = st.id' ).where( 's.id = ?', self.to_step_id ).order( 's.id' ).first
+    #end
   
   # ## A method to show if a route is current.
   # A route may have start and end dates.
-  def current
+  #def current
     
     # If the route has a start date and the start date is in the future, being today or after today ...
-    if self.start_date and self.start_date >= Date.today
+    #if self.start_date and self.start_date >= Date.today
       
       # .. we set current to false.
-      current = false
+      #current = false
       
     # Otherwise, if the route has an end date and the end date is in the past, being earlier than today ...
-    elsif self.end_date and self.end_date < Date.today
+    #elsif self.end_date and self.end_date < Date.today
       
       # ... we set current to false.
-      current = false
+      #current = false
       
     # Otherwise, if the route has no start date or a start date in the past or no end date or an end date in the future ...
-    else
+    #else
       
       # ... we set current to true.
-      current = true
-    end
+      #current = true
+      #end
     
     # We return the value we've set for the route's currency.
-    current
-  end
+    #current
+    #end
 end
