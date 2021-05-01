88ac8500f63cf6340773543b116669e123e792e8
Michael Smethurst <michael@smethur.st>
2021-05-01 17:17:01 +0100

    a touch of commenting

--- lib/parsing/route_currency.rb
+++ lib/parsing/route_currency.rb
@@ -19,34 +19,35 @@ module PARSE_ROUTE_CURRENCY
       update_route_hash( route_id, 'FALSE', 'UNTRAVERSABLE', true, nil )
     end
   end
-end
 
 
-# ## A method to show if a route is current.
-# A route may have start and end dates.
-def current_route?( route_id )
-  start_date = @routes[route_id][:route].start_date
-  end_date = @routes[route_id][:route].end_date
+  # ## A method to show if a route is current.
+  # We call the method with the ID of the route.
+  # A route may have start and end dates.
+  def current_route?( route_id )
+    start_date = @routes[route_id][:route].start_date
+    end_date = @routes[route_id][:route].end_date
   
-  # If the route has a start date and the start date is in the future, being today after today ...
-  if start_date and start_date > Date.today
+    # If the route has a start date and the start date is in the future, being today or after today ...
+    if start_date and start_date > Date.today
     
-    # .. we set current to false.
-    current = false
+      # .. we set current to false.
+      current = false
     
-  # Otherwise, if the route has an end date and the end date is in the past, being earlier than today ...
-  elsif end_date and end_date < Date.today
+    # Otherwise, if the route has an end date and the end date is in the past, being earlier than today ...
+    elsif end_date and end_date < Date.today
     
-    # ... we set current to false.
-    current = false
+      # ... we set current to false.
+      current = false
     
-  # Otherwise, if the route has no start date or a start date in the past or no end date or an end date in the future ...
-  else
+    # Otherwise, if the route has no start date or a start date in the past or no end date or an end date in the future ...
+    else
     
-    # ... we set current to true.
-    current = true
+      # ... we set current to true.
+      current = true
+    end
+  
+    # We return the value we've set for the route's currency.
+    current
   end
-  
-  # We return the value we've set for the route's currency.
-  current
 end
