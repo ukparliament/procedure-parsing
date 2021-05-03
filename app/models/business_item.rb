c10b5774c22e90dff114b39b84ef040b44b1c0f5
Michael Smethurst <michael@smethur.st>
2021-05-03 13:22:04 +0100

    new method to get steps with house count to save a query when generating house label

--- app/models/business_item.rb
+++ app/models/business_item.rb
@@ -2,6 +2,23 @@ class BusinessItem < ActiveRecord::Base
   
   has_many :actualisations
   has_many :steps, :through => 'actualisations'
+  
+  def steps_with_house_count
+    Step.find_by_sql(
+      "select s.*, count( hs1.id ) as commons_count, count( hs2.id ) as lords_count
+      from steps s
+      inner join actualisations
+      on s.id = actualisations.step_id 
+      and actualisations.business_item_id = #{self.id}
+      left join house_steps hs1
+        on s.id = hs1.step_id
+        and hs1.house_id = 1
+      left join house_steps hs2
+        on s.id = hs2.step_id
+        and hs2.house_id = 2
+      group by s.id"
+    )
+  end
 
   def web_link_domain
      if web_link
