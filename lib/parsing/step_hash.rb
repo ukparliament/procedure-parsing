ce7953809afb6ae08200f10a7875c5f89d503284
Michael Smethurst <michael@smethur.st>
2021-05-06 12:13:51 +0100

    fixed up comments with robert

--- lib/parsing/step_hash.rb
+++ lib/parsing/step_hash.rb
@@ -7,7 +7,7 @@ module PARSE_STEP_HASH
     # We create three hashes: a hash to store all steps objects in a procedure, keyed off the step ID ...
     @steps = {}
     
-    # ... a hash of IDs of outbound routes from each individual step, keyed off the step ID
+    # ... a hash of IDs of outbound routes from each individual step, keyed off the step ID ...
     @routes_from_steps = {}
     
     # ... and a hash of IDs of inbound routes to each individual step, keyed off the step ID.
@@ -30,10 +30,10 @@ module PARSE_STEP_HASH
     	@routes.each do |route|
         
         # ... we add the ID of the route to the array of outbound IDs, if the route is outbound from this step ...
-        from_route_array << route[1][:route].id if route[1][:route].from_step_id == step.id
+        from_route_array << route[0] if route[1][:route].from_step_id == step.id
         
         # ... we add the ID of the route to the array of inbound IDs, if the route is inbound to this step ...
-    		to_route_array << route[1][:route].id if route[1][:route].to_step_id == step.id
+    		to_route_array << route[0] if route[1][:route].to_step_id == step.id
     	end
       
       # ... we add the array of outbound route IDs to the hash of outbound routes, keyed off the step ID ...
@@ -47,12 +47,10 @@ module PARSE_STEP_HASH
     end
   end
   
-  ### =====
-  
   # ## A set of methods to get attributes of the steps hash.
   # We call all methods with the ID of the step.
   
-  # ### Method to get the step object from the steps hash.
+  # ### Method to get the step object from the steps hash.
   def step_object( step_id )
     @steps[step_id]
   end
@@ -62,35 +60,32 @@ module PARSE_STEP_HASH
     step_object( step_id )[:name]
   end
   
-  # ### Method to get the name of type of a step from the steps hash.
-  def step_type_name( step_id )
-    step_object( step_id )[:name]
-  end
-  
   # ### Method to check if a step has been actualised with a business item having a date in the past or of today.
   def step_has_been_actualised_has_happened?( step_id )
     
-    # We assume the step has not been with a business item having a date in the past or of today.
+    # We assume the step has not been actualised with a business item having a date in the past or of today.
     actualised_has_happened = false
     
     # We set the actualised_has_happened boolean to true if the step has been actualised in this work package, by at least one business item having a date in the past or of today.
-    actualised_has_happened = true if @steps[step_id].actualisation_has_happened_count > 0
+    actualised_has_happened = true if step_object( step_id ).actualisation_has_happened_count > 0
     
     # We return the boolean.
     actualised_has_happened
   end
   
-  # ### Method to get inbound routes to a step.
+  # ### Method to get an array of IDs of inbound routes to a step.
   def step_inbound_routes( step_id )
     @routes_to_steps[step_id]
   end
   
-  # ### Method to get the first inbound route to a step.
+  # ### Method to get the ID of the first inbound route to a step.
+  # The appearance of inbound routes in first or second place has no meaning beyond the order they are delivered from the data store.
   def step_first_inbound_route( step_id )
     step_inbound_routes( step_id )[0]
   end
   
-  # ### Method to get the second inbound route to a step.
+  # ### Method to get the ID of the second inbound route to a step.
+  # The appearance of inbound routes in first or second place has no meaning beyond the order they are delivered from the data store.
   def step_second_inbound_route( step_id )
     step_inbound_routes( step_id )[1]
   end
