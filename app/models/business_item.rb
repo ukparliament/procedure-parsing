6f4dad3f2969662aa64cdef25d9f301b60810381
Michael Smethurst <michael@smethur.st>
2021-05-12 15:13:52 +0100

    updated steps_with_house_count method to nested queries like the method in parliament_procedures model. Brings back additional sum attributes with same name

--- app/models/business_item.rb
+++ app/models/business_item.rb
@@ -5,18 +5,28 @@ class BusinessItem < ActiveRecord::Base
   
   def steps_with_house_count
     Step.find_by_sql(
-      "select s.*, count( hs1.id ) as commons_count, count( hs2.id ) as lords_count
-      from steps s
-      inner join actualisations
-      on s.id = actualisations.step_id 
-      and actualisations.business_item_id = #{self.id}
-      left join house_steps hs1
-        on s.id = hs1.step_id
-        and hs1.house_id = 1
-      left join house_steps hs2
-        on s.id = hs2.step_id
-        and hs2.house_id = 2
-      group by s.id"
+      "
+        SELECT s.*, SUM(commons_step.is_commons) AS is_in_commons, SUM(lords_step.is_lords) AS is_in_lords
+        FROM steps s
+        INNER JOIN actualisations a
+      	  ON s.id = a.step_id
+          AND a.business_item_id = #{self.id}
+        LEFT JOIN
+          (
+            SELECT 1 as is_commons, hs.step_id
+            FROM house_steps hs
+            WHERE hs.house_id = 1
+          ) commons_step
+          ON s.id = commons_step.step_id
+        LEFT JOIN
+          (
+            SELECT 1 as is_lords, hs.step_id
+            FROM house_steps hs
+            WHERE hs.house_id = 2
+          ) lords_step
+          ON s.id = lords_step.step_id
+        GROUP BY s.id;
+      "
     )
   end
 
