49f4ceab12af580128294bdf197c4401d510971b
Michael Smethurst <michael@smethur.st>
2021-12-15 16:25:42 +0000

    updated design to point to new design notes. linked to design motes for routes from a business step, expanded unactualised comment to include actaulised with no date

--- lib/parsing/business_step.rb
+++ lib/parsing/business_step.rb
@@ -4,7 +4,7 @@ module PARSE_BUSINESS_STEP
   # ## Method to parse a route whose source step is a business step.
   def parse_route_from_business_step( route_id )
   
-    # Design note: The [method used](https://ukparliament.github.io/ontologies/procedure/flowcharts/meta/design-notes/with-step-types/#validating-inputs-and-outputs-to-steps) for validating the number of input and output routes for each step type.
+    # Design note: The [method used](https://ukparliament.github.io/ontologies/procedure/maps/meta/design-notes/#validating-inputs-and-outputs-to-steps) for validating the number of input and output routes for each step type.
     # If the business step does not have one inbound route ...
     if step_inbound_routes( route_source_step_id( route_id ) ).size != 1
       
@@ -17,7 +17,8 @@ module PARSE_BUSINESS_STEP
       # ... we get the ID of the inbound route.
       inbound_route_id = step_first_inbound_route( route_source_step_id( route_id ) )
       
-      # ... if the inbound route to the source step has a status of 'UNTRAVERSABLE' ...
+      # We refer to the [design notes for assigning attributes to a route from a business step](https://ukparliament.github.io/ontologies/procedure/maps/meta/design-notes/#routes-from-business-steps).
+      # If the inbound route to the source step has a status of 'UNTRAVERSABLE' ...
       if route_is_untraversable?( inbound_route_id )
           
         # ... then “the bridge is closed” and we set the “roads off the bridge” as closed ...
@@ -33,7 +34,7 @@ module PARSE_BUSINESS_STEP
           # ... we set the route status to 'TRUE', the actualisation count to the actualisations has happened count of the business step and the parsed attribute to true.
           update_route_hash( route_id, nil, 'TRUE', true, route_source_step_actualised_has_happened_count( route_id ), nil )
             
-        # ... otherwise, the source step has not been actualised or has only been actualised by business items with dates in the future ....
+        # ... otherwise, the source step has not been actualised or has only been actualised by business items with dates in the future or by business items with no date ...
         else
             
           # ... and we set the route status to 'FALSE', the actualisation count to 0 and the parsed attribute to true.
