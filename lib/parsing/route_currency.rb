3d24276f2c438076f6c94252cdb4bd25827bd24a
Michael Smethurst <michael@smethur.st>
2021-12-15 13:04:09 +0000

    updated comments. linked to design notes on route currency

--- lib/parsing/route_currency.rb
+++ lib/parsing/route_currency.rb
@@ -1,7 +1,7 @@
-# # Module to determine whether a route is current.
+# # Module to check whether a route is current and set its current and status attributes appropriately.
 module PARSE_ROUTE_CURRENCY
 
-  # ## Method to determine whether a route is current.
+  # ## Method to check whether a route is current and set its current and status attributes appropriately.
   def parse_route_currency( route_id )
     
     # If the route is current...
@@ -14,7 +14,7 @@ module PARSE_ROUTE_CURRENCY
   else
 
       # ... we update the route current attribute to FALSE and the route status attribute to UNTRAVERSABLE.
-      # Setting the route status to UNTRAVERSABLE records that the route is not currently traversable - and is used to infer that routes which follow this route are also not currently traversable.
+      # Setting the route status to [UNTRAVERSABLE records that the route is not currently traversable - and is used to infer that routes which follow this route are also not currently traversable](https://ukparliament.github.io/ontologies/procedure/maps/meta/design-notes/#route-currentness-and-untraversability).
       # We also record this route as parsed because we don't want to visit it and attempt to parse again.
       update_route_hash( route_id, 'FALSE', 'UNTRAVERSABLE', true, nil, nil )
     end
