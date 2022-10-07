ce82f3aa5d27dac911711cd72b484de2081455e4
Michael Smethurst <michael@smethur.st>
2022-10-07 15:48:50 +0100

    business item queries now join to include step depth and order by

--- app/models/work_package.rb
+++ app/models/work_package.rb
@@ -18,15 +18,75 @@ class WorkPackage < ActiveRecord::Base
   end
   
   def business_items_that_have_happened
-    BusinessItem.all.where( 'work_package_id = ?', self).where( 'date <= ?', Date.today ).order( 'date' )
+    BusinessItem.find_by_sql(
+      "
+        SELECT bi.*, step_display_depth.display_depth as display_depth
+        FROM business_items bi, actualisations a, steps s
+        
+        /* Left join to get the step depth where present. */
+        LEFT JOIN
+          (
+            SELECT sdd.display_depth as display_depth, sdd.step_id
+            FROM step_display_depths sdd
+            WHERE sdd.parliamentary_procedure_id = #{self.parliamentary_procedure.id}
+          ) step_display_depth
+          ON s.id = step_display_depth.step_id
+          
+        WHERE bi.work_package_id = #{self.id}
+        AND bi.date <= '#{Date.today}'
+        AND bi.id = a.business_item_id
+        AND a.step_id = s.id
+        ORDER BY bi.date, display_depth;
+      "
+    )
   end
 
   def business_items_that_are_scheduled_to_happen
-    BusinessItem.all.where( 'work_package_id = ?', self).where( 'date > ?', Date.today ).order( 'date' )
+    BusinessItem.find_by_sql(
+      "
+        SELECT bi.*, step_display_depth.display_depth as display_depth
+        FROM business_items bi, actualisations a, steps s
+        
+        /* Left join to get the step depth where present. */
+        LEFT JOIN
+          (
+            SELECT sdd.display_depth as display_depth, sdd.step_id
+            FROM step_display_depths sdd
+            WHERE sdd.parliamentary_procedure_id = #{self.parliamentary_procedure.id}
+          ) step_display_depth
+          ON s.id = step_display_depth.step_id
+          
+        WHERE bi.work_package_id = #{self.id}
+        AND bi.date > '#{Date.today}'
+        AND bi.id = a.business_item_id
+        AND a.step_id = s.id
+        ORDER BY bi.date, display_depth;
+      "
+    )
   end
   
   def business_items_unknown
-    BusinessItem.all.where( 'work_package_id = ?', self).where( 'date is null' ).order( 'date' )
+    BusinessItem.find_by_sql(
+      "
+        SELECT bi.*, step_display_depth.display_depth as display_depth
+        FROM business_items bi, actualisations a, steps s
+        
+        /* Left join to get the step depth where present. */
+        LEFT JOIN
+          (
+            SELECT sdd.display_depth as display_depth, sdd.step_id
+            FROM step_display_depths sdd
+            WHERE sdd.parliamentary_procedure_id = #{self.parliamentary_procedure.id}
+          ) step_display_depth
+          ON s.id = step_display_depth.step_id
+          
+        WHERE bi.work_package_id = #{self.id}
+        AND bi.date is null
+        AND bi.id = a.business_item_id
+        AND a.step_id = s.id
+        ORDER BY bi.date, display_depth;
+      "
+    )
   end
 
   def business_items_that_have_happened_number
