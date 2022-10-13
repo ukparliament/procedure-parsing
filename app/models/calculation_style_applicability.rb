50edd057f667581e9fdc58225f897c3156cc664b
Michael Smethurst <michael@smethur.st>
2022-10-13 16:36:35 +0100

    new model for calculation style applicability

---
+++ app/models/calculation_style_applicability.rb
@@ -0,0 +1,2 @@
+class CalculationStyleApplicability < ApplicationRecord
+end
---
+++ test/models/calculation_style_applicability_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class CalculationStyleApplicabilityTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
