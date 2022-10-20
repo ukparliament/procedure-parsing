7e57ebe2f54ff6b4812efa0386752dae7847f902
Michael Smethurst <michael@smethur.st>
2022-10-20 11:39:53 +0100

    step collection types deprecated

--- app/models/step_collection_type.rb
+++
@@ -1,2 +0,0 @@
-class StepCollectionType < ApplicationRecord
-end
--- test/models/step_collection_type_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class StepCollectionTypeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
