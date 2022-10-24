67e845d9d65d0427c8ce2b91c2cbdd64ac7ec769
Michael Smethurst <michael@smethur.st>
2022-10-24 12:03:23 +0100

    added comments

--- app/models/business_item.rb
+++ app/models/business_item.rb
@@ -32,14 +32,21 @@ class BusinessItem < ActiveRecord::Base
     )
   end
   
+  # A method to return all visible steps actualised by a business item, with step depth and House memberships.
+  # We pass in the procedure in order to get the step depth in that procedure. */
   def visible_steps_with_house_count( procedure )
     Step.find_by_sql(
       "
+        /* We get the step, whether the step is in the Commons, whether the step is in the Lords and the step's display depth, where present. */
         SELECT s.*, SUM(commons_step.is_commons) AS is_in_commons, SUM(lords_step.is_lords) AS is_in_lords, step_display_depth.display_depth
         FROM steps s
+        
+        /* We only get steps with actualisations. */
         INNER JOIN actualisations a
       	  ON s.id = a.step_id
           AND a.business_item_id = #{self.id}
+          
+        /* We left join to check if the step is the Commons. */
         LEFT JOIN
           (
             SELECT 1 AS is_commons, hs.step_id
@@ -48,6 +55,8 @@ class BusinessItem < ActiveRecord::Base
             GROUP BY hs.id
           ) commons_step
           ON s.id = commons_step.step_id
+          
+        /* We left join to check if the step is in the Lords. */
         LEFT JOIN
           (
             SELECT 1 AS is_lords, hs.step_id
@@ -56,19 +65,28 @@ class BusinessItem < ActiveRecord::Base
             GROUP BY hs.id
           ) lords_step
           ON s.id = lords_step.step_id
+          
+        /* We left join to get the display depth of the step where present. */
         LEFT JOIN
           (
             SELECT s.id AS step_id, sdd.display_depth AS display_depth
             FROM steps s, step_display_depths sdd
             WHERE s.id = sdd.step_id
+            
+            /* We check that the step depth is for this procedure. */
             AND sdd.parliamentary_procedure_id = #{procedure.id}
           ) step_display_depth
           ON s.id = step_display_depth.step_id,
+        /* We join to step collection memberships and step collections */
         step_collection_memberships scm, step_collections sc
         WHERE s.id = scm.step_id
         AND scm.step_collection_id = sc.id
+        
+        /* Only returning steps in the 'Website visible steps' collection. */
         AND sc.label = 'Website visible steps'
         GROUP BY s.id, step_display_depth.display_depth
+        
+        /* We order the steps, by display depth. */
         ORDER BY step_display_depth.display_depth;
       "
     )
