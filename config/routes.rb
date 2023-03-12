0ea646d212c9678872c9f55ef19548c2fb57e1a6
fantasticlife <michael@smethur.st:wq>
2023-03-12 16:21:27 +0000

    nested under procedure-parsing

--- config/routes.rb
+++ config/routes.rb
@@ -14,27 +14,27 @@ Rails.application.routes.draw do
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
   
-  get 'procedure-parser/procedures' => 'procedure#index', as: :procedure_list
-  get 'procedure-parser/procedures/:procedure' => 'procedure#show', as: :procedure_show
-  get 'procedure-parser/procedures/:procedure/work-packages' => 'procedure#work_package_index', as: :procedure_work_package_list
-  get 'procedure-parser/procedures/:procedure/steps' => 'procedure#step_index', as: :procedure_step_list
-  get 'procedure-parser/procedures/:procedure/routes' => 'procedure#route_index', as: :procedure_route_list
+  get 'procedure-parsing/procedures' => 'procedure#index', as: :procedure_list
+  get 'procedure-parsing/procedures/:procedure' => 'procedure#show', as: :procedure_show
+  get 'procedure-parsing/procedures/:procedure/work-packages' => 'procedure#work_package_index', as: :procedure_work_package_list
+  get 'procedure-parsing/procedures/:procedure/steps' => 'procedure#step_index', as: :procedure_step_list
+  get 'procedure-parsing/procedures/:procedure/routes' => 'procedure#route_index', as: :procedure_route_list
   
-  get 'procedure-parser/work-packages/:work_package' => 'work_package#show', as: :work_package_show
-  get 'procedure-parser/work-packages/:work_package/parse' => 'work_package#parse', as: :work_package_parse
-  get 'procedure-parser/work-packages/:work_package/parse/log' => 'work_package#log', as: :work_package_log
-  get 'procedure-parser/work-packages/:work_package/parse/visualise' => 'work_package#visualise', as: :work_package_visualise
-  get 'procedure-parser/work-packages/:work_package/parse/skeleton' => 'work_package#skeleton', as: :work_package_skeleton
+  get 'procedure-parsing/work-packages/:work_package' => 'work_package#show', as: :work_package_show
+  get 'procedure-parsing/work-packages/:work_package/parse' => 'work_package#parse', as: :work_package_parse
+  get 'procedure-parsing/work-packages/:work_package/parse/log' => 'work_package#log', as: :work_package_log
+  get 'procedure-parsing/work-packages/:work_package/parse/visualise' => 'work_package#visualise', as: :work_package_visualise
+  get 'procedure-parsing/work-packages/:work_package/parse/skeleton' => 'work_package#skeleton', as: :work_package_skeleton
   
-  get 'procedure-parser/instruments/:instrument' => 'instrument#show', as: :instrument_show
+  get 'procedure-parsing/instruments/:instrument' => 'instrument#show', as: :instrument_show
   
-  get 'procedure-parser/meta' => 'meta#index', as: :meta_list
-  get 'procedure-parser/meta/schema' => 'meta#schema', as: :meta_schema
-  get 'procedure-parser/meta/comments' => 'meta#comments', as: :meta_comments
-  get 'procedure-parser/meta/bookmarklet' => 'meta#bookmarklet', as: :meta_bookmarklet
-  get 'procedure-parser/meta/link-check' => 'meta#link_check', as: :meta_link_check
-  get 'procedure-parser/meta/link-check/work-packages' => 'meta#link_check_work_package', as: :meta_link_check_work_package
-  get 'procedure-parser/meta/link-check/business-items' => 'meta#link_check_business_item', as: :meta_link_check_business_item
+  get 'procedure-parsing/meta' => 'meta#index', as: :meta_list
+  get 'procedure-parsing/meta/schema' => 'meta#schema', as: :meta_schema
+  get 'procedure-parsing/meta/comments' => 'meta#comments', as: :meta_comments
+  get 'procedure-parsing/meta/bookmarklet' => 'meta#bookmarklet', as: :meta_bookmarklet
+  get 'procedure-parsing/meta/link-check' => 'meta#link_check', as: :meta_link_check
+  get 'procedure-parsing/meta/link-check/work-packages' => 'meta#link_check_work_package', as: :meta_link_check_work_package
+  get 'procedure-parsing/meta/link-check/business-items' => 'meta#link_check_business_item', as: :meta_link_check_business_item
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
